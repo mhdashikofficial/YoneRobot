8216bb8c7ffb5ebe4fdf7055b97ddcdd7acdd19a
Muhammed Ashik <116175002+mhdashikofficial@users.noreply.github.com>
2023-10-16 10:36:07 +0530

    Update __main__.py

--- Yone/__main__.py
+++ Yone/__main__.py
@@ -83,18 +83,7 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-PM_START_TEXT = """ Hello *{}*, My name is *{}*! 
-A telegram group management bot. I'm here to help you to manage your groups.
-I have lots of handy features such as:
-‣ Warning system
-‣ Artificial intelligence
-‣ Flood control system
-‣ Note keeping system
-‣ Filters keeping system
-‣ Approvals and much more.
-
-So what are you waiting for?
-*Add me in your groups and give me full rights to make me function well.*
+PM_START_TEXT = """ Hello
 """
 
 
